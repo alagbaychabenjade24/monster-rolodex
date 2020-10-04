994a9663ddc260a3333b7e40b8a0493d502f3873
Chaben Jade Alagbay <alagbaychabenjade24@gmail.com>
2020-10-05 07:26:47 +0800

    Updates

---
+++ precache-manifest.f89d33ef0a2eeb32d95179e05fcda4da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8febe8ee1ecdc69a91b543252b50b31",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "330033b437538038e029",
+    "url": "/monster-rolodex/static/css/main.4be3aa06.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "330033b437538038e029",
+    "url": "/monster-rolodex/static/js/main.d69ab415.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
